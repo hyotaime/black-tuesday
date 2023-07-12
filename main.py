dc80d834a3e524e3f5c3054dd517ba8e6cb7e79b
hyotaime <hyotaime@gmail.com>
2023-07-12 18:42:53 +0900

    fix: /help

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info("help")
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Command List\n/help - Show command list\n/find - Search stock information at investing.com\n/now - Show real time price of the stock\n"
+        text="Command List\n/help - Show command list\n/find - Search stock information at investing.com\n/now - Show real time price of the stock\n/alarm - Set NASDAQ, KOSPI, NIKKEI price alarm\n/search - Search anything you want at google.com\n/gpt - Ask anything to chatGPT"
     )
 
 def searchCrawling(search_value):
@@ -278,7 +278,5 @@ if __name__ == '__main__':
 
     gpt_handler = CommandHandler('gpt', gpt)
     application.add_handler(gpt_handler)
-
-
 
     application.run_polling()
